8c4843efb3660f7125a29c3326f8e88e2dd855a4
condy <jhoncarlo.condy+dev@gmail.com>
2024-09-12 16:03:11 +0800

    initial commit

---
+++ app/Http/Controllers/GeoInfoController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GeoInfoController extends Controller
+{
+    public function getGeoLocation(Request $request)
+    {
+
+    }
+}
